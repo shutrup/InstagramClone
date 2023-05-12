adb640ea5429527a4505e2e079e246cd3e4a2097
shutrup <magagaraev95@mail.ru>
2023-05-12 22:00:23 +0300

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Шарап Бамматов on 12.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
